b67ff2b04f5b522c00dec5432416f89fcb33a564
Vincent Feng <ivincentfeng@live.com>
2016-04-12 17:51:58 +0800

    Misc\StairsClimbing   + StairsClimbing

---
+++ Misc/StairsClimbing/StairsClimbing.cs
@@ -0,0 +1,28 @@
+﻿namespace Dsna.Misc.StairsClimbing
+{
+    public class StairsClimbing
+    {
+        public ulong ClimbStairs(uint stairsSteps)
+        {
+            if (stairsSteps <= 2)
+            {
+                return stairsSteps;
+            }
+
+            ulong prev1 = 1;
+            ulong prev2 = 2;
+
+            ulong result = 0;
+
+            for (int ix = 3; ix <= stairsSteps; ix++)
+            {
+                result = prev1 + prev2;
+
+                prev1 = prev2;
+                prev2 = result;
+            }
+
+            return result;
+        }
+    }
+}
