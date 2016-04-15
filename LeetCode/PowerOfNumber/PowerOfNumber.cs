b4f0578613d6f646b0ce54bb36b073443cf98494
iVincentFeng <iVincentFeng@live.com>
2016-04-15 15:36:23 +0800

    LeetCode\PowerOfNumber   + PowerOfNumber   + PowerOfNumberUt

---
+++ LeetCode/PowerOfNumber/PowerOfNumber.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Dsna.LeetCode.PowerOfNumber
+{
+    /// <summary>
+    /// https://leetcode.com/problems/powx-n/
+    /// </summary>
+    /// <notes>
+    ///   1. Equality of doubles
+    ///   2. What if x is zero and n is negative?
+    ///   3. What is x is zero and n is not zero?
+    ///   4. What if x is not zero and n is negative?
+    ///   5. What if x is not zero and n is int.MinValue?
+    ///   6. What if x is not zero and n is zero?
+    ///   7. What if the result exceeds [double.MinValue, double.MaxValue]?
+    /// </notes>
+    public class PowerOfNumber
+    {
+        public double Power(double number, int power)
+        {
+            if (this.IsEqual(number, 0.00d))
+            {
+                if (power < 0)
+                {
+                    throw new ArgumentException();
+                }
+                else
+                {
+                    return 0.00d;
+                }
+            }
+            else
+            {
+                if (power == 0)
+                {
+                    return 1.00d;
+                }
+            }
+
+            long longPower = power;
+            
+            bool isNegativePower = (longPower < 0);
+            if (isNegativePower)
+            {
+                longPower = -longPower;
+            }
+
+            double result = this.DoPower(number, longPower);
+
+            if (isNegativePower)
+            {
+                result = 1.00d / result;
+            }
+
+            return result;
+        }
+
+        private double DoPower(double number, long power)
+        {
+            if (power == 0)
+            {
+                return 1.00d;
+            }
+
+            if (power == 1)
+            {
+                return number;
+            }
+
+            double result = this.DoPower(number, power / 2);
+            result *= result;
+            if (power % 2 != 0)
+            {
+                result *= number;
+            }
+
+            return result;
+        }
+
+        private bool IsEqual(double x, double y)
+        {
+            if (Math.Abs(x - y) < 0.000001)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LeetCode/PowerOfNumber/PowerOfNumberUt.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Dsna.LeetCode.PowerOfNumber
+{
+    [TestClass()]
+    public class PowerOfNumberUt
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Test_Power_When_NumberIsZeroAndPowerIsLessThanZero()
+        {
+            this.powerOfNumber.Power(0.00d, -1);
+        }
+
+        [TestMethod()]
+        public void Test_Power_When_NumberIsZeroAndPowerIsEqualToOrGreaterThanZero()
+        {
+            Assert.AreEqual<double>(0.00d, this.powerOfNumber.Power(0.00d, 0));
+            Assert.AreEqual<double>(0.00d, this.powerOfNumber.Power(0.00d, 1));
+        }
+
+        [TestMethod()]
+        public void Test_Power_When_NumberIsNotZeroAndPowerIsZero()
+        {
+            Assert.AreEqual<double>(1.00d, this.powerOfNumber.Power(8.00d, 0));
+        }
+
+        [TestMethod()]
+        public void Test_Power_When_NumberIsNotZeroAndPowerIsPositive()
+        {
+            Assert.AreEqual<double>(8.00d, this.powerOfNumber.Power(2.00d, 3));
+        }
+
+        [TestMethod()]
+        public void Test_Power_When_NumberIsNotZeroAndPowerIsNegative()
+        {
+            Assert.AreEqual<double>(0.125d, this.powerOfNumber.Power(2.00d, -3));
+        }
+
+        private PowerOfNumber powerOfNumber = new PowerOfNumber();
+    }
+}
